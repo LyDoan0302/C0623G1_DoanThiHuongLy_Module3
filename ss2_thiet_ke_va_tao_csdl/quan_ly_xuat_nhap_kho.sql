399bba62418a78f1badbe8ae442527014639a730
LyDoan0302 <138420217+LyDoan0302@users.noreply.github.com>
2023-09-13 14:01:49 +0700

    up

--- ss2_thiet_ke_va_tao_csdl/quan_ly_xuat_nhap_kho.sql
+++ ss2_thiet_ke_va_tao_csdl/quan_ly_xuat_nhap_kho.sql
@@ -1,54 +1,54 @@
-create database quan_ly_xuat_nhap_kho;
-use quan_ly_xuat_nhap_kho;
-create table vat_tu(
-ma_vat_tu int auto_increment primary key,
-ten_vat_tu varchar(50)
+CREATE DATABASE quan_ly_xuat_nhap_kho;
+USE quan_ly_xuat_nhap_kho;
+CREATE TABLE vat_tu(
+ma_vat_tu INT AUTO_INCREMENT PRIMARY KEY,
+ten_vat_tu VARCHAR(50)
 );
-create table phieu_nhap(
-so_pn int auto_increment primary key,
-ngay_nhap datetime
+CREATE TABLE phieu_nhap(
+so_pn INT AUTO_INCREMENT PRIMARY KEY,
+ngay_nhap DATETIME
 );
-create table phieu_xuat(
-so_px int auto_increment primary key,
-ngay_xuat datetime
+CREATE TABLE phieu_xuat(
+so_px INT AUTO_INCREMENT PRIMARY KEY,
+ngay_xuat DATETIME
 );
-create table chi_tiet_phieu_nhap_kho(
-so_pn int,
-ma_vat_tu int,
-primary key(so_pn,ma_vat_tu),
-foreign key(so_pn) references phieu_nhap(so_pn),
-foreign key(ma_vat_tu) references vat_tu(ma_vat_tu),
-don_gia_nhap int,
-so_luong_nhap int
+CREATE TABLE chi_tiet_phieu_nhap_kho(
+so_pn INT,
+ma_vat_tu INT,
+PRIMARY KEY(so_pn,ma_vat_tu),
+FOREIGN KEY(so_pn) REFERENCES phieu_nhap(so_pn),
+FOREIGN KEY(ma_vat_tu) REFERENCES vat_tu(ma_vat_tu),
+don_gia_nhap INT,
+so_luong_nhap INT
 );
-create table chi_tiet_phieu_xuat_kho(
-so_px int,
-ma_vat_tu int,
-primary key(so_px,ma_vat_tu),
-foreign key(so_px) references phieu_xuat(so_px),
-foreign key(ma_vat_tu) references vat_tu(ma_vat_tu),
-don_gia_xuat int,
-so_luong_xuat int
+CREATE TABLE chi_tiet_phieu_xuat_kho(
+so_px INT,
+ma_vat_tu INT,
+PRIMARY KEY(so_px,ma_vat_tu),
+FOREIGN KEY(so_px) REFERENCES phieu_xuat(so_px),
+FOREIGN KEY(ma_vat_tu) REFERENCES vat_tu(ma_vat_tu),
+don_gia_xuat INT,
+so_luong_xuat INT
 );
-create table don_dat_hang(
-so_DH int auto_increment primary key,
-ngay_dat_hang datetime
+CREATE TABLE don_dat_hang(
+so_DH INT AUTO_INCREMENT PRIMARY KEY,
+ngay_dat_hang DATETIME
 );
-create table chi_tiet_don_dat_hang(
-so_DH int,
-ma_vat_tu int,
-primary key(so_DH,ma_vat_tu),
-foreign key(so_DH) references don_dat_hang(so_DH),
-foreign key(ma_vat_tu) references vat_tu(ma_vat_tu)
+CREATE TABLE chi_tiet_don_dat_hang(
+so_DH INT,
+ma_vat_tu INT,
+PRIMARY KEY(so_DH,ma_vat_tu),
+FOREIGN KEY(so_DH) REFERENCES don_dat_hang(so_DH),
+FOREIGN KEY(ma_vat_tu) REFERENCES vat_tu(ma_vat_tu)
 );
-create table nha_cung_cap(
-ma_NCC varchar(20) primary key,
-ten_NCC varchar(50),
-dia_chi varchar(50)
+CREATE TABLE nha_cung_cap(
+ma_NCC VARCHAR(20) PRIMARY KEY,
+ten_NCC VARCHAR(50),
+dia_chi VARCHAR(50)
 );
-create table so_dien_thoai(
-id_SDT int primary key, 
-ma_NCC varchar(20),
-so_dien_thoai varchar(10),
-foreign key(ma_NCC) references nha_cung_cap(ma_NCC)
+CREATE TABLE so_dien_thoai(
+id_SDT INT PRIMARY KEY, 
+ma_NCC VARCHAR(20),
+so_dien_thoai VARCHAR(10),
+FOREIGN KEY(ma_NCC) REFERENCES nha_cung_cap(ma_NCC)
 );
