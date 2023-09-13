ba30f7560e8421087ac0605dfdecc5967ab87391
LyDoan0302 <138420217+LyDoan0302@users.noreply.github.com>
2023-09-13 14:08:28 +0700

    up_bai_sua

--- ss1_co_so_du_lieu_quan_he/student_management.sql
+++ ss1_co_so_du_lieu_quan_he/student_management.sql
@@ -1,22 +1,23 @@
-create database student_management;
-use student_management;
-create table Student(
- id int,
- student_name varchar(200),
- age int,
- country varchar(50)
+CREATE DATABASE student_management;
+USE student_management;
+CREATE TABLE Student(
+ id INT PRIMARY KEY,
+ student_name VARCHAR(200),
+ age INT,
+ country VARCHAR(50)
 );
-create table class(
-id int primary key auto_increment,
-class_name varchar(55) 
+CREATE TABLE class(
+id INT PRIMARY KEY AUTO_INCREMENT,
+class_name VARCHAR(55) 
 );
-create table teacher(
-id int primary key auto_increment,
-teacher_name varchar(200),
-age int,
-country varchar(50)
+CREATE TABLE teacher(
+id INT PRIMARY KEY AUTO_INCREMENT,
+teacher_name VARCHAR(200),
+age INT,
+country VARCHAR(50)
 
 );
-select * from `student`;
-alter table `class` RENAME `Class_name`;
-alter table `Class_name` RENAME `Class`;
+SELECT * 
+FROM `student`;
+ALTER TABLE `class` RENAME `Class_name`;
+ALTER TABLE `Class_name` RENAME `Class`;
